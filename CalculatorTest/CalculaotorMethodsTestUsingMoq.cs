4b842ae3ae08760a21efa640830e0b93f85163ab
Pratyusha Chirravuri <Pratyusha_Chirravuri@epam.com>
2020-01-27 11:51:16 +0530

    Add project files.

---
+++ CalculatorTest/CalculaotorMethodsTestUsingMoq.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Moq;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculaotorMethodsTestUsingMoq
+    {
+
+        [TestMethod]
+        public void AddSimpleTestMoq()
+        {
+            var mock = new Mock<Calculator.ICalculatorMethods>();
+            mock.Setup(x => x.add(29, 2)).Returns(31);
+            mock.Setup(x => x.add(9, -1)).Returns(8);
+
+        }
+
+        [TestMethod]
+        public void divideByZeroMoq()
+        {
+            var mock = new Mock<Calculator.ICalculatorMethods>();
+            mock.Setup(cal => cal.divide(1, 0)).Throws<DivideByZeroException>();
+        }
+
+        [TestMethod]
+        public void subtractArgumentCheckMoq()
+        {
+            var mock = new Mock<Calculator.ICalculatorMethods>();
+            mock.Setup(cal => cal.subtract(It.IsAny<Int32>(),It.IsAny<Int32>())).Returns(1);
+        }
+
+
+    }
+}
